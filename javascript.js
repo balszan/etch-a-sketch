aa1903ff47c2ff1eb7ae00cc341d062acd3111ce
balszan <kbalszan@gmail.com>
2023-09-02 11:36:22 +0100

    Add custom square number functionality

--- javascript.js
+++ javascript.js
@@ -1,18 +1,23 @@
 const board = document.querySelector('.board');
 board.style.gridTemplateColumns = 'repeat(16,1fr)';
 board.style.gridTemplateRows = 'repeat(16,1fr)';
-
-
+const button = document.querySelector('#choice');
 
 function createSquares(num) {
     for(let i=0; i<num**2; i++) {
         let square = document.createElement('div');
         square.classList.add('squares');
         board.appendChild(square);
-    
+        board.style.gridTemplateColumns = `repeat(${num},1fr)`;
+        board.style.gridTemplateRows = `repeat(${num},1fr)`;
     }
 }
 
+function removeSquares() {
+    while(board.firstChild) {
+        board.removeChild(board.firstChild);
+    }
+}
 
 createSquares(16);
 
@@ -21,7 +26,11 @@ const squares = document.querySelectorAll('.squares');
 for(let i=0; i<squares.length; i++) {
     squares[i].addEventListener('mouseover', () => {
         squares[i].style.backgroundColor = 'black';
-        console.log(i);
     })
 }
 
+button.addEventListener('click', () => {
+    let gridNumber = prompt('How many squares on each side?');
+    removeSquares();
+    createSquares(gridNumber);
+})
