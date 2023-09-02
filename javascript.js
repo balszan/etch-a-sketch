122ae779f51ac019b9d030e2a98d970ebe66e315
balszan <kbalszan@gmail.com>
2023-09-02 09:06:06 +0100

    Add basic structure

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#container');
+
+for(let i=0; i<16; i++) {
+    let square = document.createElement('div');
+    container.appendChild(square);
+    square.classList.add('squares');
+}
